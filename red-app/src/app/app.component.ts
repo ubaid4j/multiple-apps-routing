6686d2e9aa8699de686c8471d521b5a98c5b2f7b
ubaid4j <ubaidshan007@gmail.com>
2024-07-21 10:41:21 +0500

    - trigger red app build

--- red-app/src/app/app.component.ts
+++ red-app/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {UserinfoComponent} from "./userinfo/userinfo.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
+    selector: 'app-root',
+    standalone: true,
     imports: [RouterOutlet, UserinfoComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'red-app';
+    title = 'red-app';
 }
